fd1b43c815bd8ae26632394f1224679f0fb41b32
Wei ZHANG <wei_zhang_1988@outlook.com>
2023-10-17 16:18:29 +0800

    work on loopclosing

--- src/loopClosing.hpp
+++ src/loopClosing.hpp
@@ -20,10 +20,84 @@
 #include <memory>
 
 namespace YDORBSLAM{
+  class Tracking;
+  class LocalMapping;
+  class KeyFrameDatabase;
   class LoopClosing{
     public:
-    LoopClosing();
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    //consistent group is a set of connected key frames of current key frame
+    //and number of times of it being continuously found in groups of previous key frame
+    typedef std::pair<std::set<std::shared_ptr<KeyFrame>>,int> KeyFrameAndNum;
     typedef std::map<std::shared_ptr<KeyFrame>,g2o::Sim3,std::less<std::shared_ptr<KeyFrame>>,Eigen::aligned_allocator<std::pair<const std::shared_ptr<KeyFrame>, g2o::Sim3>>> KeyFrameAndPose;
+    LoopClosing(std::shared_ptr<Map> _sptr_map, std::shared_ptr<KeyFrameDatabase> _sptr_keyFrameDB, std::shared_ptr<DBoW3::Vocabulary> _sptr_voc, const bool &_b_isScaleFixed);
+    inline void setTracker(std::shared_ptr<Tracking> _sptr_tracker){
+      m_sptr_tracker = _sptr_tracker;
+    }
+    inline void setLocalMapper(std::shared_ptr<LocalMapping> _sptr_localMapper){
+      m_sptr_localMapper = _sptr_localMapper;
+    }
+    //main function
+    void run();
+    void insertKeyFrame(std::shared_ptr<KeyFrame> _sptr_keyFrame);
+    void requestReset();
+    //this function will run in a separate thread
+    void runGlobalBundleAdjustment(long int _int_loopKeyFrameID);
+    inline bool isRunningGlobalBA(){
+      std::unique_lock<std::mutex> lock(m_mutex_globalBA);
+      return m_b_isRunningGlobalBA;
+    }
+    inline bool isGlobalBAFinished(){
+      std::unique_lock<std::mutex> lock(m_mutex_globalBA);
+      return m_b_isGlobalBAFinished;
+    }
+    inline void requestFinish(){
+      std::unique_lock<std::mutex> lock(m_mutex_finish);
+      m_b_isFinishRequested = true;
+    }
+    inline bool isFinished(){
+      std::unique_lock<std::mutex> lock(m_mutex_finish);
+      return m_b_isFinished;
+    }
+    protected:
+    bool checkNewKeyFrames();
+    bool detectLoop();
+    bool computeSim3();
+    void searchAndFuse(const KeyFrameAndPose &_correctedPosesMap);
+    void correctLoop();
+    void resetIfRequested();
+    inline bool isFinishRequested(){
+      std::unique_lock<std::mutex> lock(m_mutex_finish);
+      return m_b_isFinishRequested;
+    }
+    inline void setFinish(){
+      std::unique_lock<std::mutex> lock(m_mutex_finish);
+      m_b_isFinished = true;
+    }
+    bool m_b_isResetRequested;
+    bool m_b_isFinishRequested;
+    bool m_b_isFinished;
+    std::shared_ptr<Map> m_sptr_map;
+    std::shared_ptr<Tracking> m_sptr_tracker;
+    std::shared_ptr<KeyFrameDatabase> m_sptr_keyFrameDB;
+    std::shared_ptr<DBoW3::Vocabulary> m_sptr_voc;
+    std::shared_ptr<LocalMapping> m_sptr_localMapper;
+    std::list<std::shared_ptr<KeyFrame>> m_list_keyFrameBufferQueue;
+    float m_int_connectionConsistencyThd;
+    std::shared_ptr<KeyFrame> m_sptr_currentLoopKeyFrame;
+    std::shared_ptr<KeyFrame> m_sptr_currentMatchedKeyFrame;
+    std::vector<KeyFrameAndNum> m_v_lastConsistentGroups;
+    std::vector<std::shared_ptr<KeyFrame>> m_v_consistentCandidates;
+    std::vector<std::shared_ptr<KeyFrame>> m_v_connectedKeyFrames;
+    std::vector<std::shared_ptr<MapPoint>> m_v_matchedMapPoints;
+    std::vector<std::shared_ptr<MapPoint>> m_v_loopMapPoints;
+    cv::Mat m_cvMat_sim3_c2w;
+    g2o::Sim3 m_g2o_sim3_c2w;
+    long int m_int_lastLoopKeyFrameID;
+    int m_int_fullBAIndex;
+    bool m_b_isRunningGlobalBA, m_b_isGlobalBAFinished, m_b_isGlobalBAStopped, m_b_isScaleFixed;
+    std::mutex m_mutex_reset, m_mutex_finish, m_mutex_loopQueue, m_mutex_globalBA;
+    std::shared_ptr<std::thread> m_sptr_globalBAThread;
   };
 }//namespace YDORBSLAM
 
